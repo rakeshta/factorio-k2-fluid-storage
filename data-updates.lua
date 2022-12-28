20dcdd4784642e70ca7ec0a1a84118d6d04832d6
Rakesh Ayyaswami <rakeshta@gmail.com>
2022-12-29 04:28:22 +1100

    allow placement in space in Space Exploration mod

---
+++ data-updates.lua
@@ -0,0 +1,4 @@
+if mods["space-exploration"] then
+  data.raw["storage-tank"]["kr-fluid-storage-1"].se_allow_in_space = true
+  -- data.raw["storage-tank"]["kr-fluid-storage-2"].se_allow_in_space = true
+end
