01f621e47f85c536657db1419af889a9ca6b0886
Rakesh Ayyaswami <rakeshta@gmail.com>
2022-12-29 15:31:11 +1100

    enable large storage tank in space

--- data-updates.lua
+++ data-updates.lua
@@ -1,5 +1,5 @@
 -- enable fluid storage in space
 if mods["space-exploration"] then
   data.raw["storage-tank"]["kr-fluid-storage-1"].se_allow_in_space = true
-  -- data.raw["storage-tank"]["kr-fluid-storage-2"].se_allow_in_space = true
+  data.raw["storage-tank"]["kr-fluid-storage-2"].se_allow_in_space = true
 end
